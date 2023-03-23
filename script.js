d15e919155b41ccf6d68282e47b5b548fbedb3ac
MH-Alikhani <mohammad.hosein.alikhani08@gmail.com>
2023-03-23 16:05:25 -0700

    Create  contact & footer

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 document.querySelector(".hamburger-menu").addEventListener("click", () => {
   document.querySelector(".container").classList.toggle("change");
 });
+
+// back to top button
+document.querySelector(".scroll-btn").addEventListener("click", () => {
+  document.querySelector("html").style.scrollBehavior = "smooth";
+  setTimeout(() => {
+    document.querySelector("html").style.scrollBehavior = "unset";
+  }, 1000);
+});
