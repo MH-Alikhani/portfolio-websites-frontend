1e4c09c5e4acda056086d41e640fd991ad217950
MH-Alikhani <mohammad.hosein.alikhani08@gmail.com>
2023-03-23 16:36:53 -0700

    Make wesbite responsive and create its loader

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+window.onload = () => {
+  setTimeout(() => {
+    document.querySelector("body").classList.add("display");
+  }, 4000);
+};
+
 // open or close header menu
 document.querySelector(".hamburger-menu").addEventListener("click", () => {
   document.querySelector(".container").classList.toggle("change");
